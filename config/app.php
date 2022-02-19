79cb7dd59a80d7b196dc3a4520134bd414ebeb5d
Prasad K <prasad.k@krds.fr>
2022-02-20 03:27:02 +0530

    changes

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
-$config['AWS_ACCESS_KEY'] = 'XXXX';
-$config['AWS_SECRET_KEY'] = 'YYYY';
-$config['AWS_REGION'] = 'us-east-1';
+$config['AWS_ACCESS_KEY'] = env('AWS_ACCESS_KEY');
+$config['AWS_SECRET_KEY'] = env('AWS_SECRET_KEY');
+$config['AWS_REGION'] = env('AWS_REGION');
 
 return $config;
